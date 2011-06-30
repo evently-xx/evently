7608f7ac80f8d2a4d1f295deefc1ff400ec71254
dzhou <dr.dingzhou@gmail.com>
2011-06-29 17:33:20 -0700

    core class definition

---
+++ evently/core/models.py
@@ -0,0 +1,31 @@
+'''
+data abstraction
+
+@author dzhou
+'''
+
+__author__ = 'dzhou'
+
+
+from django.db import models
+
+class Event(models.Model):
+  '''
+  an event is the occasion, the idea, and the reasons for someone to
+  organize an event
+  '''
+
+  timeBegin = models.DateTimeField(db_index=True)
+  timeEnd = models.DateTimeField(db_index=True)
+  title = models.CharField(max_length=1024)
+  description = models.TextField()
+
+
+class Meetup(models.Model):
+  '''
+  a meetup is a gathering of friends to attend an event.
+  it is a temporary association between a group of people with an event
+  '''
+
+  eventId = models.IntegerField()
+  userIds = models.TextField() # better field type ?
--- evently/urls.py
+++ evently/urls.py
@@ -9,8 +9,9 @@ urlpatterns = patterns('',
     # (r'^evently/', include('evently.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
+
 )
